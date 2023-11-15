fd8a8f28b61f2c9855d1dfb1c1d322e560c1495f
Kuhuk Agarwal <79625787+kuhuk521@users.noreply.github.com>
2023-11-15 23:32:27 +0530

    first

--- content.js
+++ content.js
@@ -85,7 +85,7 @@ httpRequest.onreadystatechange = function() {
 };
 httpRequest.open(
   "GET",
-  "https://5d76bf96515d1a0014085cf9.mockapi.io/product",
+  "https://5d76bf6515d1a0014085cf9.mockapi.io/product",
   true
 );
 httpRequest.send();
